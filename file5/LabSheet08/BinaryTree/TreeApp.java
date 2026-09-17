a23222dcae493c1551b95f79f818a8c7a84182b8
Mekmongkol Kamonpun <2613110218@tni.ac.th>
2026-09-17 17:57:55 +0700

    2613110218 submit Lab09

---
+++ file5/LabSheet08/BinaryTree/TreeApp.java
@@ -0,0 +1,32 @@
+package BinaryTree;
+
+public class TreeApp {
+
+	public static void main(String[] args) {
+		System.out.println("------------------------------");
+		System.out.println("Binary Tree from createTree1()");
+		System.out.println("------------------------------");
+		
+		BinaryTree tree = new BinaryTree();
+		tree.createTree1();
+		tree.printTree(tree.getRoot(), 0);
+		
+		
+			System.out.println("------------------------------");
+			System.out.println("Binary Tree from createTree2()");
+			System.out.println("------------------------------");
+			
+			BinaryTree tree2 = new BinaryTree();
+			tree.createTree2();
+			tree.printTree(tree.getRoot(), 0);
+			
+			System.out.println("------------------------------");
+			System.out.println("Binary Tree from createTree3()");
+			System.out.println("------------------------------");
+			
+			BinaryTree tree3 = new BinaryTree();
+			tree.createTree3();
+			tree.printTree(tree.getRoot(), 0);
+	}
+
+}
